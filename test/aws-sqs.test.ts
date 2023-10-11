179e987fc20829d1aaf50bde2606dbf76e1edf94
magweng <magwengg@gmail.com>
2023-10-11 09:16:59 +0200

    feat: init cdk

---
+++ test/aws-sqs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsSqs from '../lib/aws-sqs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-sqs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsSqs.AwsSqsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
